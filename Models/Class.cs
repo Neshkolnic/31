cdbe44b6b1884b5ca2887b543949c9d9c9b192b7
Admin <Admin@DESKTOP-TS0M18F>
2024-05-23 10:05:09 +0300

    Добавьте файлы проекта.

---
+++ Models/Class.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MobileStore.Models;
+using WebApplication1.Models;
+using System.Linq;
+using MobileStore.Models;
+
+namespace MobileStore.Models
+{
+    public class MobileContext : DbContext
+    {
+        public DbSet<Phone> Phones { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public MobileContext(DbContextOptions<MobileContext> options)
+        : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
+
+
+
+namespace MobileStore
+{
+    public static class SampleData
+    {
+        public static void Initialize(MobileContext context)
+        {
+            if (!context.Phones.Any())
+            {
+                context.Phones.AddRange(
+                new Phone
+                {
+                    Name = "iPhone X",
+                    Company = "Apple",
+                    Price = 600
+                },
+                new Phone
+                {
+                    Name = "Samsung Galaxy Edge",
+                    Company = "Samsung",
+                    Price = 550
+                },
+                new Phone
+                {
+                    Name = "Pixel 3",
+                    Company = "Google",
+
+                    Price = 500
+                }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
+
